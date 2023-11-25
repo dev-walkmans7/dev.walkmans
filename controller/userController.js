fad1fc0631c691ab4629b42225ad6daf877d834b
Aritra Dutta <aritrababi2002@gmail.com>
2023-11-26 00:30:04 +0530

    hi

--- controller/userController.js
+++ controller/userController.js
@@ -8,7 +8,7 @@ const roleRepo = require("../repositories/roleRepo.js");
 const config = require("../config/config.js");
 const mailer = require("../helper/mailer.js");
 const User = new userModel();
-//HI
+//HII
 
 class userController {
   constructor() {}
